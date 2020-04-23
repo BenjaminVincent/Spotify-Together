efb3b96034b6e96963b0ed6148ea188cb05dce63
BenjaminVincent <32846783+BenjaminVincent@users.noreply.github.com>
2020-04-23 13:22:28 -0700

    working on socketio implementation

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require("cors");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/testAPI");
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const testAPIRouter = require("./routes/testAPI");
 
 
-var app = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,11 +27,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/testAPI", testAPIRouter);
+app.use('/testAPI', testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
+});
+
+io.on('connection', (socket)=> {
+  console.log("a user has connected");
+});
+
+server.listen(80, () => {
+  console.log("server listening on port: 80");
 });
 
 // error handler
