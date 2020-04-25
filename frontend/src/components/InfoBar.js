682f6528c44b63856864fdc4e7b5781839e360bb
BenjaminVincent <32846783+BenjaminVincent@users.noreply.github.com>
2020-04-25 16:43:04 -0700

    cleaned up InfoBar

--- frontend/src/components/InfoBar.js
+++ frontend/src/components/InfoBar.js
@@ -3,16 +3,16 @@ import React from 'react';
 import onlineIcon from '../icons/onlineIcon.png';
 import closeIcon from '../icons/closeIcon.png';
 
-import "../styles/InfoBar.css";
+import '../styles/InfoBar.css';
 
 const InfoBar = ({ room }) => (
   <div>
     <div>
-      <img className="onlineIcon" src={onlineIcon} alt="online icon" />
+      <img className='onlineIcon' src={onlineIcon} alt='online' />
       <h3>{room}</h3>
     </div>
     <div>
-      <a href="/"><img src={closeIcon} alt="close icon" /></a>
+      <a href='/'><img src={closeIcon} alt='close' /></a>
     </div>
   </div>
 );
