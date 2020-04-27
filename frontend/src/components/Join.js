74983c5ef702e5069fb739da2bd9bd83aff179c6
BenjaminVincent <32846783+BenjaminVincent@users.noreply.github.com>
2020-04-27 15:10:52 -0700

    updated join redirect

--- frontend/src/components/Join.js
+++ frontend/src/components/Join.js
@@ -13,7 +13,7 @@ const Join = (props) => {
                 <div><input placeholder="Display Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)}/></div><br/>
                 <div><input placeholder="Room ID" className="joinInput"type="text" onChange={(event) => setRoom(event.target.value)}/></div><br/>
                 <div>
-                    <Link onClick={(event) => (!name || !room) ? event.preventDefault() : null} to={`/join?name=${name}&room=${room}`}>
+                    <Link onClick={(event) => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
                         <button className="btn bton--loginApp-link" type="submit">join</button>
                     </Link>
                 </div>
