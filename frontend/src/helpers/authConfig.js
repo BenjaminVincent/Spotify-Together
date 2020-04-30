ba7b435c00ee6de6dd4ed30cbf85a0b9cd39d2cf
BenjaminVincent <32846783+BenjaminVincent@users.noreply.github.com>
2020-04-30 16:50:27 -0700

    cleaned-up helpers

--- frontend/src/helpers/authConfig.js
+++ frontend/src/helpers/authConfig.js
@@ -1,9 +1,9 @@
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
 // Replace with your app's client ID, redirect URI and desired scopes
-export const clientId = "5c2e565bf4f448e395b0062ec854dd1b";
-export const redirectUri = "http://localhost:3000/";
+export const clientId = '5c2e565bf4f448e395b0062ec854dd1b';
+export const redirectUri = 'http://localhost:3000/';
 export const scopes = [
-  "user-read-currently-playing",
-  "user-read-playback-state",
-  "user-modify-playback-state",
+  'user-read-currently-playing',
+  'user-read-playback-state',
+  'user-modify-playback-state',
 ];
