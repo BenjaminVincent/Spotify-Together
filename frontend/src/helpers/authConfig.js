55f2e68b6b129ecf47acaab3dcebe1433f5e46ad
BenjaminVincent <32846783+BenjaminVincent@users.noreply.github.com>
2020-05-05 18:29:13 -0700

    fixing deploy files

--- frontend/src/helpers/authConfig.js
+++ frontend/src/helpers/authConfig.js
@@ -1,9 +1,11 @@
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = '5c2e565bf4f448e395b0062ec854dd1b';
-export const redirectUri = 'https://listen-together-music.herokuapp.com/';
+export const redirectUri = 'http://5eb1fb2f4e503485ed7d44cd--listen-together.netlify.app/';
 export const scopes = [
   'user-read-currently-playing',
   'user-read-playback-state',
   'user-modify-playback-state',
 ];
+
+// 'https://listen-together-music.herokuapp.com/'
