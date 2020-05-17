9eea190cbaf02297d7a318094d9dcbdd7021e39c
nmateyko <34617861+nmateyko@users.noreply.github.com>
2020-05-17 10:39:33 -0700

    host name remains when message sent

--- server/index.js
+++ server/index.js
@@ -41,7 +41,6 @@ io.on('connection', (socket)=> {
     socket.on('sendMessage', (message, callback) => {
       const user = getUser(socket.id);
       io.to(user.room).emit('message', { user: user.name, text: message });
-      io.to(user.room).emit('roomData', { room: user.room, text: message });
   
       callback();
     });
