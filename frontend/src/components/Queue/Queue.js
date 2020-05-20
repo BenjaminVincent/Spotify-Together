04a62bc0a090cfcb6317beaf0b3356a987fd9e4e
BenjaminVincent <32846783+BenjaminVincent@users.noreply.github.com>
2020-05-20 16:01:13 -0700

    installed search WIP

--- frontend/src/components/Queue/Queue.js
+++ frontend/src/components/Queue/Queue.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import QueueItem from './QueueItem';
-
+import Search from './Search';
 /*
     Queue is intended to show all the songs that the host has added to the queue.
     Play the next song in the queue.
@@ -16,16 +16,13 @@ const Queue = ({song, artist, image}) => {
 
   const queueList = [];
   return (
-  <div className='queue-container'>
-    <div className='queue-title'>Up Next: </div>
-    <div><QueueItem song={song} artist={artist} image={image}/></div>
-    <div><QueueItem song={song} artist={artist} image={image}/></div>
-    <div><QueueItem song={song} artist={artist} image={image}/></div>
-    <div><QueueItem song={song} artist={artist} image={image}/></div>
-    <div><QueueItem song={song} artist={artist} image={image}/></div>
-    <div><QueueItem song={song} artist={artist} image={image}/></div>
+  // <div className='queue-container'>
+  //   <div className='queue-title'>Up Next: </div>
+  //   <div><QueueItem song={song} artist={artist} image={image}/></div>
+  //   <Search/>
+  // </div>
 
-  </div>
+<Search/>
   );
 }
 
--- frontend/src/components/Queue/Search.js
+++ frontend/src/components/Queue/Search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactSearchBox from 'react-search-box';
 
 /*
     Search uses: https://developer.spotify.com/documentation/web-api/reference/search/search/ 
@@ -11,8 +12,37 @@ import React from 'react';
 */
 
 const Search = () => {
+  const data = [
+    {
+      key: 'john',
+      value: 'John Doe',
+    },
+    {
+      key: 'jane',
+      value: 'Jane Doe',
+    },
+    {
+      key: 'mary',
+      value: 'Mary Phillips',
+    },
+    {
+      key: 'robert',
+      value: 'Robert',
+    },
+    {
+      key: 'karius',
+      value: 'Karius',
+    },
+  ]
+
   return (
-  <div>Search</div>
+  <div>      
+    <ReactSearchBox
+    placeholder="Search..."
+    data={data}
+    callback={record => console.log(record)}
+    />
+  </div>
   );
 }
 
