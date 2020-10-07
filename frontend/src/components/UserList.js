d140202a81e46c1bf819b0f925ee968a4bacbbb8
BenjaminVincent <32846783+BenjaminVincent@users.noreply.github.com>
2020-10-06 20:44:08 -0700

    fixed styling with chat

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { FaUserCircle } from 'react-icons/fa';
 import '../styles/UserList.css';
 
+const truncateText = (text, len) => {
+  if (text.length > len) return text.slice(0, len) + '...';
+  else return text.slice(0, len);
+}
+
 const UserList = ({ users }) => {
 
   let listUsers = users.map(user => (
 
     <li className='user-image' key={user.id}>
-        <FaUserCircle size='1.2em'/>
-        <span className='user-name'>{user.name}</span>
+        <FaUserCircle size='30px'/>
+        <span className='user-name'>{truncateText(user.name, 7)}</span>
     </li>
   ));
 
